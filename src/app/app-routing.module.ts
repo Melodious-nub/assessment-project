d16853d46dbf6e57ab92d5dd8e9a8f9367c01789
Melodious-nub <shawon.taluckder1@gmail.com>
2024-11-04 12:05:35 +0600

    updated with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './services/auth.guard';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { SignUpComponent } from './pages/auth/sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: LoginComponent, pathMatch: 'full'},
+  { path:'signup', component: SignUpComponent},
+
+  // Lazzy Loading Route 1
+  {
+    path: 'admin',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+    import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+
+  // wildCard for notfound
+  // { path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
